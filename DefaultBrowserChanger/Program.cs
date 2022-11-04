7701c76bebc4ab9998e4cbf9dc1d7fdbcbc2e5e9
Nedavnii Viacheslav <vny@tobii.com>
2022-11-04 18:55:09 +0100

    Rewritten to MinimalApi
    Added context menu with close app action
    
    Signed-off-by: Nedavnii Viacheslav <vny@tobii.com>

--- DefaultBrowserChanger/Program.cs
+++ DefaultBrowserChanger/Program.cs
@@ -1,69 +1,75 @@
 using System.Diagnostics;
 
-namespace DefaultBrowserChanger;
-internal static class Program
+NotifyIcon? _browserIcon = null;
+
+ApplicationConfiguration.Initialize();
+
+UpdateBrowserIcon();
+
+Application.Run();
+
+void ChangeBrowserHandler(object? sender, MouseEventArgs e)
 {
-    private static NotifyIcon? _browserIcon;
-
-    [STAThread]
-    private static void Main()
+    if (e.Button != MouseButtons.Left)
     {
-        ApplicationConfiguration.Initialize();
-
-        UpdateBrowserIcon();
-
-        Application.Run();
+        return;
     }
 
-    private static void ChangeBrowserHandler(object? sender, EventArgs e)
+    var currentBrowser = CallBrowserScriptCommand("Get-CurrentDefaultBrowser");
+    switch (currentBrowser)
     {
-        var currentBrowser = CallBrowserScriptCommand("Get-CurrentDefaultBrowser");
-
-        switch (currentBrowser)
-        {
-            case "Chrome":
-                CallBrowserScriptCommand("Set-DefaultBrowserByName", "Firefox");
-                break;
-            case "Firefox":
-                CallBrowserScriptCommand("Set-DefaultBrowserByName", "Chrome");
-                break;
-            default:
-                throw new InvalidProgramException($"Unknown current browser {currentBrowser}");
-        }
-
-        UpdateBrowserIcon();
+        case "Chrome":
+            CallBrowserScriptCommand("Set-DefaultBrowserByName", "Firefox");
+            break;
+        case "Firefox":
+            CallBrowserScriptCommand("Set-DefaultBrowserByName", "Chrome");
+            break;
+        default:
+            throw new InvalidProgramException($"Unknown current browser {currentBrowser}");
     }
 
-    private static void UpdateBrowserIcon()
+    UpdateBrowserIcon();
+}
+
+void UpdateBrowserIcon()
+{
+    if (_browserIcon != null)
     {
-        if (_browserIcon != null)
-        {
-            _browserIcon.Click -= ChangeBrowserHandler;
-            _browserIcon.Dispose();
-        }
-
-        var browser = CallBrowserScriptCommand("Get-CurrentDefaultBrowser");
-
-        _browserIcon = new NotifyIcon
-        {
-            Visible = true,
-            Icon = new Icon($"{browser}.ico")
-        };
-        _browserIcon.Click += ChangeBrowserHandler;
+        _browserIcon.MouseClick -= ChangeBrowserHandler;
+        _browserIcon.Visible = false;
+        _browserIcon.ContextMenuStrip.Dispose();
+        _browserIcon.Dispose();
     }
 
-    private static string CallBrowserScriptCommand(params string[] commands)
-    {
-        var processInfo = new ProcessStartInfo("powershell.exe")
-        {
-            CreateNoWindow = true,
-            UseShellExecute = false,
-            RedirectStandardOutput = true,
-            Arguments = $"-File BrowserUtils.ps1 {string.Join(' ', commands.Select(c => $"\"{c}\""))}"
-        };
+    var browser = CallBrowserScriptCommand("Get-CurrentDefaultBrowser");
 
-        using var process = Process.Start(processInfo);
-        process!.WaitForExit();
-        return process.StandardOutput.ReadToEnd().Trim();
+    _browserIcon = new NotifyIcon
+    {
+        Visible = true,
+        Icon = new Icon($"{browser}.ico"),
+        ContextMenuStrip = MakeContextMenu()
+    };
+    _browserIcon.MouseClick += ChangeBrowserHandler;
+
+    ContextMenuStrip MakeContextMenu()
+    {
+        var menu = new ContextMenuStrip();
+        menu.Items.Add("Close", null, (_, _) => Application.Exit());
+        return menu;
     }
+}
+
+string CallBrowserScriptCommand(params string[] commands)
+{
+    var processInfo = new ProcessStartInfo("powershell.exe")
+    {
+        CreateNoWindow = true,
+        UseShellExecute = false,
+        RedirectStandardOutput = true,
+        Arguments = $"-File BrowserUtils.ps1 {string.Join(' ', commands.Select(c => $"\"{c}\""))}"
+    };
+
+    using var process = Process.Start(processInfo);
+    process!.WaitForExit();
+    return process.StandardOutput.ReadToEnd().Trim();
 }
